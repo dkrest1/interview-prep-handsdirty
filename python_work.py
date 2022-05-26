ab6334010679eba12f6b7651f7d2c3807e73d425
Akande Oluwatosin <aktosin1@gmail.com>
2022-05-26 04:23:55 +0100

    set up profile

---
+++ python_work.py
@@ -0,0 +1,274 @@
+# print("languages: \n\tPython\n\tC\n\tJavascript")
+# language = "python "
+# print(language)
+# print(language.rstrip())
+# language
+
+# name = "Tosin"
+# print("Hello " + name)
+
+
+# name  = "Oluwatosin Akande"
+# print(name.title())
+# print(name.lower())
+# print(name.upper())
+
+
+# print('Albert Einstein once said "what ever you do, do it well"')
+
+# name = "ade"
+# age = 23
+
+# print("Happy " + str(age) + "rd"+ " Birthday " + name )
+
+# print(8 - 5)
+# print(3 + 5)
+# print(16 / 2)
+# print(4 * 2)
+
+# lis = ["bicycle", "canoe", "okada"]
+# print(lis)
+# print(lis[0].upper())
+
+# my_friends = ["Segun", "Ikayh", "William", "Aj", "Juwon", "Brian", "Jendo"]
+# print("my first frient is {}".format(my_friends[0].upper()))
+# print("my second friend is {}".format(my_friends[1].upper()))
+# print("my third friend is {}".format(my_friends[2].upper()))
+# print("my fourth friend is {}".format(my_friends[3].upper()))
+# print("my fifth friend is".format(my_friends[4].upper()))
+# print("my sisth friend is {}".format(my_friends[5].upper()))
+# print("my seventh friend is {}".format(my_friends[6].upper()))
+# my_friends.append("Psalm")
+# print(my_friends)
+
+# my_exes = []
+# my_exes.append("ruka")
+# my_exes.append("toyin")
+# my_exes.append("bukky")
+# my_exes.append("lola")
+# print(my_exes)
+
+# motocycles = ["honda", "toyota", "bus"]
+# motocycles.insert(1, "ole")
+# motocycles.insert(2, "omomi")
+# print(motocycles)
+# del motocycles[1]
+# print(motocycles)
+# del motocycles[1]
+# print(motocycles)
+# print(motocycles)
+# motocycles.append("toyota")
+# print(motocycles)
+# lis = motocycles.pop()
+# print(motocycles)
+# print(lis)
+# lis = motocycles.pop()
+# print(lis)
+# print(motocycles)
+# motocycles.append("tosin")
+# motocycles.append("akande")
+# motocycles.pop(1)
+# print(motocycles)
+# motocycles.remove("tosin")
+# print(motocycles)
+# motocycles = ["honda", "yamaha", "suzuki", "ducati"]
+# print(motocycles)
+
+# too_expensive = "ducati"
+# motocycles.remove(too_expensive)
+# print(motocycles)
+# print("\nA " + too_expensive.title() + "is too expensive for me")
+
+
+# guest_list = ["Ikayh", "Segun", "Enique"]
+# print("{} I am inviting you for my dinner".format(guest_list[0]))
+# print("{} please accept my invitation for the dinner".format(guest_list[1]))
+# print("{} wao I can even do without inviting you to the dinner".format(
+#     guest_list[2]))
+# print(guest_list)
+# cant_makeit = guest_list.pop(1)
+# print(cant_makeit)
+# replace_segun = guest_list.insert(2, "Habib")
+# print(replace_segun)
+# print(replace_segun)
+# print("wao {} can make it to the list so, I'm inviting {} to the dinner".format(
+#     cant_makeit, guest_list[2]))
+
+# guest_list.append("Jendo")
+# print(guest_list)
+# guest_list.pop(1)
+# guest_list.pop(1)
+# guest_list.pop(1)
+# print(guest_list)
+# guest_list.pop(1)
+# print(guest_list)
+
+# sort_cars = cars.sort()
+# print(sort_cars)
+# cars.sort()
+# print(cars)
+# cars.sort(reverse=True)
+# print(cars)
+
+# print("here is the original list of the cars")
+# print(cars)
+# print("\n here is the temporary storage of cars")
+# print(sorted(cars))
+# print("\n here is the the original list")
+# print(cars)
+
+# tempo_sort = sorted(cars)
+# print(tempo_sort)
+
+# cars = ["bmw", "audi", "toyota", "subaru"]
+# print(cars)
+# cars.reverse()
+# print(cars)
+# print(cars)
+# cars.reverse()
+# print(cars)
+
+# places = ["satorini", "maldives", "miami", "paris", "sigapore"]
+# print(places)
+# print(sorted(places))
+# print(places)
+# print(sorted(places).reverse())
+# places.reverse()
+# print(places)
+# places.reverse()
+# print(places)
+# places.sort()
+# print(places)
+# places.sort()
+# print(places)
+# places.sort()
+# print(places)
+# places.sort(reverse=True)
+
+# print(places)
+# print(places)
+# print(places[5])
+# print(places[4])
+
+
+# magicians = ["ikayh", "segun", "mohammed", "ola"]
+# for magician in magicians:
+#     print(magician.title() + " you people are doing a great work")
+#     print(magician.title() + " we are currently looking up to you next magic \n")
+# print("thank you all for your participation")
+
+
+# message = "hello python world"
+# print(message)
+
+# magicians = ["olu", "ikayh", "tosin", "ade"]
+# for magician in magicians:
+#     print("you are a great magician " + magician)
+#     print("thank you all for all you people do")
+
+
+# list_pizza = ["dodo-pizza", "iyan-pizza", "rice-pizza"]
+
+# for each in list_pizza:
+#     print("{} has a great taste".format(each))
+# print("I so much love pizza like mad \n")
+# print("it's all I love and I can't do without it \n")
+# print("pizza is a good food I love it")
+
+# animals = ["dog", "cat", "rabbit"]
+# for animal in animals:
+#     print("A " + animal + " will make a good pet")
+# print("all the animals will make a good pet though")
+
+# for value in range(0, 5):
+#     print(value)
+
+# list_numbers = list(range(1, 6))
+# print(list_numbers)
+
+# range_even = [list(range(2, 12, 2))]
+# print(range_even)
+
+# squares = []
+# for value in range(1, 6):
+#     square = value ** 2
+#     squares.append(square)
+# print(squares)
+# numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+# print((sum(numbers)))
+# squares = [value ** 2 for value in range(1, 6)]
+# print(squares)
+
+
+# for i in range(1, 21):
+#     print(i)
+
+# for i in range(1, 1000000):
+#     print(i)
+
+# odds = [value for value in range(1, 31, 3)]
+# for i in odds:
+#     print(i)
+
+# num_cube = [cubes ** 3 for cubes in range(1, 11)]
+# print(num_cube)
+# for i in num_cube:
+#     print(i)
+# print(num_cube[0:3])
+
+
+# players = ["segun", "ikayh", "enique", "ajose", "habib"]
+# print("here are the first three players of my team: ")
+# for player in players[:3]:
+#     print(player.title())
+
+
+# favourite_foods = ["pizza", "rice", "beans", "eba", "dodo"]
+# print("my favourite food are: ")
+# print(favourite_foods)
+# print("my friends favourite food also are: \n")
+# print(favourite_foods[:])
+
+
+# fav_foods = ["rice", "dodo", "egg", "beans"]
+# fri_fav_foods = fav_foods[:]
+# fav_foods.append("amala")
+# fri_fav_foods.append("ice-cream")
+# print(fav_foods)
+# print(fri_fav_foods)
+
+
+# fav_foods = ["rice", "beans", "egg"]
+# fri_fav_foods = fav_foods
+# print(fav_foods)
+# print(fri_fav_foods)
+# fav_foods.append("amala")
+# print(fav_foods)
+# print(fri_fav_foods)
+
+
+# friends = ["ade", "shola", "mide", "kunle", "loveth", "seun", "temi"]
+# print("the first three friend are")
+# for first_three in friends[4:]:
+#     print(first_three)
+
+# tuple
+# tup = (200, 300)
+# print(tup[0])
+# print(tup[1])
+# for i in tup:
+#     print(i)
+
+
+# dimension = (200, 300)
+# for i in dimension:
+#     print(i)
+# print("this is the new dimesion")
+# dimension = (400, 500)
+# for i in dimension:
+#     print(i)
+
+
+basic_food = ("rice", "beans", "egg")
+for i in basic_food:
+    print(i)
