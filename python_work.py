6bf8331987ac3bf861549ee05817c82d9051fb92
Akande Oluwatosin <aktosin1@gmail.com>
2022-05-30 13:22:04 +0100

    still working on the chapter 8

--- python_work.py
+++ python_work.py
@@ -269,6 +269,517 @@
 #     print(i)
 
 
-basic_food = ("rice", "beans", "egg")
-for i in basic_food:
-    print(i)
+# basic_food = ("rice", "beans", "egg")
+# for i in basic_food:
+#     print(i)
+
+
+# IF STATEMENT
+# cars = ["audi", "bmw", "subaru", "toyota"]
+# for car in cars:
+#     if car == "bmw":
+#         print(car.upper())
+#     else:
+#         print(car.title())
+
+# car = "Audi"
+# car.lower() == "audi"
+
+# != checking
+# pizza = "eleja pizza"
+# if pizza != "mosa pizza":
+#     print("please hold the mosa ")
+
+# age = 18
+# age == 18
+
+# answer = 20
+# if answer != 47:
+#     print("that is not the correct answer")
+# friends = ["segun", "ikayh", "william", "aj"]
+# result = "ade" in friends
+# print(result)
+
+
+# babes = ["rose", "bukky", "toyin"]
+# babe = "lola"
+# if babe not in babes:
+#     print("yes you are correct, you know them all")
+
+# age = 17
+# if age >= 18:
+#     print("yes you can vote bro")
+#     print("have  you reg for vote yet ?")
+# else:
+#     print("you are too young to vote")
+#     print("as soon as you turn 18, you can vote")
+
+
+# age = 12
+# if age < 4:
+#     print("your admission into the park is frree")
+# elif age < 18:
+#     print("your admission cost $5")
+# else:
+#     print("your admission cost $10")
+
+
+# age = 12
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 5
+# elif age < 64:
+#     price = 10
+# else:
+#     price = 5
+# print("your admission cost $" + str(price) + ".")
+
+
+# fruits = ["mango", "pawpaw", "banana", "cherry", "apple"]
+# if "mango" in fruits:
+#     print("give me one mango \n")
+# if "banana" in fruits:
+#     print("add two banana to it \n")
+# if "honey" not in fruits:
+#     print("why didnt you people have honey\n")
+
+# print("Thanks give me my fruits")
+
+# alien = "green"
+# if alien == "green":
+#     print("player earn 5 points")
+# elif alien == "yellow":
+#     print("player earn 10 points")
+# elif alien == "red":
+#     print("player earn 15 points")
+# else:
+#     print("wao! none of your program work")
+# requested_toppings = ["mushroom", "green pepper", "extra cheese"]
+# for requested_topping in requested_toppings:
+#     print("adding the {}.".format(requested_topping))
+# print("Thanks , your requested pizza is ready plus added {}, {}, {}.".format(
+#     requested_toppings[0], requested_toppings[1], requested_toppings[2]))
+
+# requested_toppings = ["mushroom", "green pepper", "extra cheese"]
+# for requested_topping in requested_toppings:
+#     if requested_topping == "green pepper":
+#         print("sorry we are out of green pepper")
+#     else:
+#         print("Adding {}".format(requested_topping))
+# print("thanks your pizza is ready")
+
+
+# requested_toppings = ["pepper"]
+# if requested_toppings:
+#     for requested_topping in requested_toppings:
+#         print("adding {} to your pizza".format(requested_topping))
+#         print("thanks your pizaa is ready")
+# else:
+#     print("are you sure you really want an empty pizza")
+
+
+# available = ["rice", "eba", "beans", "spag",
+#              "iyan", "fufu", "eba", "jollof", "fried"]
+# requested = ["rice", "beans", "dodo"]
+# for each in requested:
+#     if each in available:
+#         print("adding {} to your menu". format(each))
+#     else:
+#         print("sorry {} is not available".format(each))
+# print("Thanks for patronizing us")
+
+
+# usernames = ["ade", "segun", "tola", "balo", "bukky", "motush", "admin"]
+# usernames = []
+# if usernames:
+
+#     for username in usernames:
+#         if username == "admin":
+#             print("good morning {}, do you want the name of everyone and things that happen yesterday on the group".format(username))
+#         else:
+#             print("hello {}, you are login! what do you have for us".format(username))
+# else:
+#     print("sorry no one is logged in")
+
+
+# current_users = ["segun", "ikayh", "mohammed", "habib", "dami"]
+# new_users = ["Segun", "Tosin", "enique"]
+# for new_user in new_users:
+#     if new_user.lower() in current_users:
+#         print(
+#             "sorry username {} already exist, please enter a new username".format(new_user))
+#     else:
+#         current_users.append(new_user.lower())
+#         print("User name added successfully!")
+# print("Thanks for visiting our websites")
+# print(current_users)
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# for number in numbers:
+#     if number == 1 or number == 2 or number == 3:
+#         print("{}st\n".format(number))
+#     else:
+#         print("{}th\n".format(number))
+
+# new dimension pthon crash course chapter six
+# alien_0 = {
+#     "color": "green",
+#     "point": 5}
+
+# print(alien_0["color"])
+# print(alien_0["point"])
+# print("you just earned a {} points".format(str(alien_0["point"])))
+# alien_0["x-position"] = 0
+# alien_0["y-position"] = 25
+# print(alien_0)
+
+# alien_0 = {}
+# alien_0["color"] = "blue"
+# alien_0["point"] = 5
+# print(alien_0)
+# alien_0["color"] = "yellow"
+# print([alien_0])
+# print("the alien color is now {}".format(alien_0["color"]))
+# alien_0 = {"color": "red", "point": 5, "x-position": 20,
+#            "y-position": 5, "speed": "medium"}
+# print("original x position = {}".format(str(alien_0["x-position"])))
+
+# move the alien to the right
+# determine the position of the alien
+# if alien_0["speed"] == "slow":
+#     x_incerement = 1
+# elif alien_0["speed"] == "medium":
+#     x_incerement = 2
+# else:
+#     x_incerement = 3
+
+
+# alien_new_position = alien_0["x-position"] + x_incerement
+
+# print("the new alien position is {} ".format(alien_new_position))
+
+
+# to del a key value pair in dictionary use the "del" keyword
+# alien_0 = {"point": 5, "color": "yellow"}
+# del alien_0["point"]
+# print(alien_0)
+# favourite_languages = {
+#     "jen": "python",
+#     "segun": "java",
+#     "timi": "c",
+#     "ikayh": "python"
+# }
+
+# print("the favourite language of time is {}".format(
+#     favourite_languages["ikayh"].title()))
+
+# details = {
+#     "first_name": "Oluwatosin",
+#     "last-name": "akande",
+#     "age": 30,
+#     "city": "osogbo"
+
+# }
+
+# print(details["first_name"])
+# print(details["last-name"])
+# print(details["age"])
+# print(details["city"])
+
+
+# poll = {
+#     "ikayh": 23,
+#     "segun": 24,
+#     "mohammed": 22
+# }
+
+# print(
+#     "Ikay: " + str(poll["ikayh"]) + "\n"
+#     "Segun: " + str(poll["segun"]) + "\n"
+#     "Mohammed: " + str(poll["mohammed"])
+# )
+
+
+# glossary = {
+#     "list": "this is used to store python value",
+#     "dictionary": "this is used to store key vaue pairs",
+#     "strings": "they are used for storing variables",
+#     "numbers": "there are different numbers in python",
+#     "float": "we have a floating point number",
+#     "comparison ==": "the comparison operator check or compare two values",
+#     "assignment =": "the assignment operator is used to assign variables to a container",
+#     "the addition + ": "the addition operator is used to sum up two numbers or to concatenate",
+#     "the int method": "the int method is used to convert variables to interger",
+#     "the str": "the str method is used to convert numbers to string",
+
+# }
+
+
+# for key, value in glossary.items():
+#     print("{}:\n{}\n".format(key.title(), value))
+
+
+# rivers = {
+#     "eygpt": "River Nile",
+#     "nigeria": "pacific ocean",
+#     "niger": "benue"
+# }
+
+# print("river Nile runs through " + rivers["eygpt"])
+# print("river pacific runs through " + rivers["nigeria"])
+# print("river Niger runs through " + rivers["benue"])
+
+# for key, value in rivers.items():
+#     print("{} :  {}".format(key.title(), value.format()))
+
+# print(glossary)
+# print("{} \n {}".format("list", glossary["list"]))
+# print("{} \n {}".format("Dictionary", glossary["dictionary"]))
+# print("{} \n {}".format("strings", glossary["strings"]))
+# print("{} \n {}".format("Numbers", glossary["numbers"]))
+# print("{} \n {}".format("Float", glossary["float"]))
+
+# users = {
+#     "username": "dkrest1",
+#     "first": "akande",
+#     "last": "oluwatosin"
+# }
+
+# for key, value in users.items():
+#     print("\nKey " + key)
+#     print("Value " + value)
+
+
+# favourites_language = {
+#     "ikay": "python",
+#     "tosin": "javascript",
+#     "segun": "python",
+#     "ade": "c++"
+# }
+# for value in favourites_language.values():
+#     print(value)
+# for key in sorted(favourites_language.items()):
+#     print("{}".format(key))
+
+
+# alien_0 = {'color': "red", "point": 5}
+# alien_1 = {"color": "black", "point": 10}
+# alien_2 = {"color": "brown", "point": 15}
+
+# aliens = [alien_0, alien_1, alien_2]
+# for alien in aliens:
+#     print(alien)
+
+# aliens = []
+# for alien in range(30):
+#     new_alien = {"color": "blue", "point": 5}
+#     aliens.append(new_alien)
+
+# for alien in aliens[:5]:
+#     print(alien)
+
+# print("the total number aliens is " + str(len(aliens)))
+
+
+# aliens = []
+# for alien in range(0, 31):
+#     new_alien = {"color": "yellow", "point": 5, "speed": "slow"}
+#     aliens.append(new_alien)
+
+# for alien in aliens[0:3]:
+#     if alien["color"] == "yellow":
+#         alien["color"] = "green"
+#         alien["point"] = "10"
+#         alien["speed"] = "medium"
+#     elif alien["color"] == "yellow":
+#         alien["color"] = "red"
+#         alien["point"] = 15
+#         alien["speed"] = "fast"
+
+
+# for alien in aliens[0:5]:
+#     print(alien)
+
+# print(aliens)
+
+# pizza = {
+#     "crust": "thick",
+#     "topping": ["onion", "cream", "banana"]
+
+# }
+
+# print(
+#     "you ordered for the following pizza {} with the following toppings".format(pizza["crust"]))
+
+# for topping in pizza["topping"]:
+#     print("\t{}".format(topping))
+
+
+# fav_languages = {
+#     "ikay": ["python", "java"],
+#     "tosin": ["javascript", "python"],
+#     "kofo": ["golang"],
+#     "shedrack": ["nodejs", "javascript"]
+# }
+
+# for name, languages in fav_languages.items():
+#     print("{}'s favourite language/languages are".format(name.upper()))
+#     for language in languages:
+#         print("\t{}".format(language.title()))
+
+
+# users_details = {
+#     "ikayh": {
+#         "username": "ikay_01",
+#         "sex": "male",
+#         "full_name": "ejiofor ikechukwu"
+#     },
+#     "segun": {
+#         "username": "sheg_10",
+#         "sex": "male",
+#         "full_name": "adesola oluwasegun"
+#     },
+#     "tosin": {
+#         "username": "dkrest1",
+#         "sex": "female",
+#         "full_name": "akande oluwatosin"
+#     }
+# }
+
+# for user, details in users_details.items():
+#     print("{}'s full details are \n".format(user.title()))
+#     for key, value in details.items():
+#         print("\t{}: {}\n".format(key, value))
+
+
+# user = input("enter your username")
+# print(user)
+
+
+# prompt = "please enter your username: "
+# prompt += "\nand your username: "
+
+# username = input(prompt)
+# print(username)
+
+
+# height = input("please insert your height in here: ")
+# height = int(height)
+
+# if height >= 30:
+#     print("you are old enough to hike")
+# else:
+#     print("sorry you aint old enough to hike")
+
+
+# try it yourself
+# car = input("please enter the maker of your car: ")
+# print("let me see if I can find {} for you".format(car))
+
+
+# num = input("how many people are in these dinner: ")
+# num = int(num)
+# if num > 8:
+#     print("I'm afraid you might have to wait")
+# else:
+#     print("you can enter as we have accommodation for your number")
+
+
+# current_number = 1
+# while current_number <= 5:
+#     print(current_number)
+#     current_number += 1
+
+# prompt = "tell me something: "
+# message = ""
+# while message != "quit":
+#     message = input(prompt)
+#     if message != "quit":
+#         print(message)
+
+
+# active = True
+
+# while active:
+#     message = input("tell me something and i will print it back to you: ")
+#     if message == "quit":
+#         active = False
+#     else:
+#         print(message)
+
+
+# while True:
+#     message = input("please enter the city you would love to go: ")
+#     if message == "quit":
+#         break
+#     else:
+#         print("I would love to go to this {}".format(message))
+
+# current_number = 0
+# while current_number < 10:
+#     current_number += 1
+#     if current_number % 2 == 0:
+#         continue
+#     print(current_number)
+
+# x = 1
+# while x <= 5:
+#     print(x)
+#     x += 1
+
+
+# active = True
+# while active:
+#     message = input("please enter the toppings you want on your pizza: ")
+#     if message == "quit":
+#         active = False
+#     else:
+#         print("you have added {} to your pizza toppings".format(message))
+
+
+# active = True
+
+# while active:
+#     age = int(input("tell your age to check the discount on your ticket: "))
+#     if age <= 3:
+#         print("your movie ticket is free")
+#     elif 3 <= age <= 12:
+#         print("your movie ticket is $10")
+#     else:
+#         print("ticket movie is $15")
+
+# uncomfirm_users = ["ade", "sam", "ikay"]
+# comfirm_users = []
+
+# while uncomfirm_users:
+#     current_user = uncomfirm_users.pop()
+#     print("verifying the current user {}".format(current_user.title()))
+#     comfirm_users.append(current_user)
+#     # displaying all comfirmed users
+# print("the following users have been comfirmed")
+# for comfirm_user in comfirm_users:
+#     print(comfirm_user)
+
+# pets = ["dog", "cat", "fish", "dog", "cat", "rabbit"]
+# print(pets)
+# while "cat" in pets:
+#     pets.remove("cat")
+# print(pets)
+
+
+responses = {}
+polling_active = True
+while polling_active:
+    name = input("please in your name:")
+    response = input("which mountain would you like: ")
+    responses[name] = response
+    message = input("do you still want to say more city: (yes/no): ")
+    if message == "no":
+        polling_active = False
+    print("thanks for voting, your result will be available shortly")
+
+print("-----result loading....-------")
+for name, response in responses.items():
+    print("\n{} : \n{}".format(name, response))
